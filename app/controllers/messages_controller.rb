2dc99be4a32bcf2fd80c64adff91ce31f0376ad9
rory jackson <roryjamesjackson@hotmail.com>
2023-05-20 08:31:39 +0100

    added some basic styling to differentiate sender/reciever

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,8 +8,10 @@ class MessagesController < ApplicationController
     if @message.save
       ChatroomChannel.broadcast_to(
         @chatroom,
-        render_to_string(partial: "message", locals: {message: @message})
+        message: render_to_string(partial: "message", locals: { message: @message }),
+        sender_id: @message.user.id
       )
+
       head :ok
     else
       render "chatrooms/show", status: :unprocessable_entity
