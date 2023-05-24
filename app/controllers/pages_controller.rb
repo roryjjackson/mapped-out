726ad4128503220e57c504aa059b46efce8aecd3
rory jackson <roryjamesjackson@hotmail.com>
2023-05-24 17:51:11 +0100

    added rating to dashboard for questionnaire

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,16 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @questionnaire = Questionnaire.where(user_id: current_user.id)
+    # raise
+    # @data = {}
+    # @questionnaires.each do |questionnaire|
+    #   user = User.find(questionnaire.user_id)
+    #   total_score = questionnaire.total_score
+    #   @questionnaire_data[questionnaire.id] = {
+    #     user: user,
+    #     total_score: total_score
+    #   }
+    # end
   end
 end
