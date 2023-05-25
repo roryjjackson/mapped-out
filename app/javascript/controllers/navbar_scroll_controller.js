6649be9691e572a33a8d061a157abe27d1ff682b
rory jackson <roryjamesjackson@hotmail.com>
2023-05-25 12:39:40 +0100

    scroll dissapear function built for navbar using stimulus

---
+++ app/javascript/controllers/navbar_scroll_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from 'stimulus';
+
+export default class extends Controller {
+  static targets = ['navbar'];
+
+  connect() {
+    this.prevScrollPos = window.pageYOffset;
+    this.navbarHeight = this.navbarTarget.offsetHeight;
+
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
+
+  disconnect() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    const currentScrollPos = window.pageYOffset;
+    const scrollingUp = currentScrollPos < this.prevScrollPos;
+
+    if (scrollingUp) {
+      this.navbarTarget.classList.remove = 'hidden';
+    } else {
+      this.navbarTarget.classList.add = `hidden`;
+    }
+
+    this.prevScrollPos = currentScrollPos;
+  }
+}
