8ac78cab505d8861aee34b3a4c499f06ef3baca2
rory jackson <roryjamesjackson@hotmail.com>
2023-05-26 10:59:10 +0100

    implemented some basic logic to identify 5 similar form submissions based on accumulated variation from each euclidean value difference

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,42 +1,47 @@
 class QuestionnairesController < ApplicationController
-  before_action :set_questionnaire, only: %i[ show edit update destroy ]
+  before_action :set_questionnaire, only: %i[edit update destroy]
 
   def total_score
     @questionnaire = Questionnaire.find(params[:id])
     authorize @questionnaire
   end
 
-  # GET /questionnaires or /questionnaires.json
   def index
     @questionnaires = policy_scope(Questionnaire)
   end
 
-  # GET /questionnaires/1 or /questionnaires/1.json
   def show
+    @questionnaire = Questionnaire.where(user_id: current_user.id).first
     authorize @questionnaire
   end
 
-  # GET /questionnaires/new
   def new
     @questionnaire = Questionnaire.new
     authorize @questionnaire
   end
 
-  # GET /questionnaires/1/edit
   def edit
     authorize @questionnaire
   end
 
-  # POST /questionnaires or /questionnaires.json
   def create
     @questionnaire = Questionnaire.new(questionnaire_params)
     @questionnaire.user = current_user
-    total = @questionnaire.q1 + @questionnaire.q2 + @questionnaire.q3 + @questionnaire.q4 + @questionnaire.q5
-    @total_score = (total.to_f / 500) * 100
-    @questionnaire.total_score = @total_score
+    # total = @questionnaire.q1 + @questionnaire.q2 + @questionnaire.q3 + @questionnaire.q4 + @questionnaire.q5
+    # @total_score = (total.to_f / 500) * 100
+    # @questionnaire.total_score = @total_score
     authorize @questionnaire
     respond_to do |format|
       if @questionnaire.save
+
+        scores = Questionnaire.where.not(id: @questionnaire.id).map do |other_questionnaire|
+          similarity_score = similarity_score(@questionnaire, other_questionnaire)
+          [similarity_score, other_questionnaire]
+        end
+
+        scores.sort!.reverse!
+
+        @most_similar = scores.first(5)
         format.html { redirect_to questionnaire_url(@questionnaire), notice: "Questionnaire was successfully created." }
         format.json { render :show, status: :created, location: @questionnaire }
       else
@@ -46,7 +51,6 @@ class QuestionnairesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /questionnaires/1 or /questionnaires/1.json
   def update
     authorize @questionnaire
 
@@ -61,7 +65,6 @@ class QuestionnairesController < ApplicationController
     end
   end
 
-  # DELETE /questionnaires/1 or /questionnaires/1.json
   def destroy
     authorize @questionnaire
 
@@ -74,13 +77,24 @@ class QuestionnairesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_questionnaire
-      @questionnaire = Questionnaire.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def questionnaire_params
-      params.require(:questionnaire).permit(:q1, :q2, :q3, :q4, :q5)
+  def set_questionnaire
+    @questionnaire = Questionnaire.find(params[:id])
+  end
+
+  def questionnaire_params
+    params.require(:questionnaire).permit(:q1, :q2, :q3, :q4, :q5)
+  end
+
+  def euclidean_distance(a, b)
+    (a - b).abs
+  end
+
+  def similarity_score(a, b)
+    total_deviation = [a.q1, a.q2, a.q3, a.q4, a.q5].zip([b.q1, b.q2, b.q3, a.q4, a.q5]).sum do |x, y|
+      euclidean_distance(x, y)
     end
+    total_deviation_percentage = (total_deviation.to_f / (a.attribute_names.size - 1) * 4) * 100
+    100 - total_deviation_percentage
+  end
 end
