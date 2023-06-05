37e4b17158f7cb799416952a4aa99eda48ce6bc0
rory jackson <roryjamesjackson@hotmail.com>
2023-06-05 16:36:00 +0100

    added functionailty to partial to stop users filling review form in twice

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_26_104245) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_152915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -121,6 +121,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_26_104245) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "profile_id", null: false
+    t.string "content"
     t.index ["profile_id"], name: "index_reviews_on_profile_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
