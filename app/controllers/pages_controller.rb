114ff9550866fdb2edd8d97a4fa051b203ddfc65
rory jackson <roryjamesjackson@hotmail.com>
2023-06-15 17:47:02 +0100

    Text design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,11 +8,12 @@ class PagesController < ApplicationController
     @questionnaire = Questionnaire.where(user_id: current_user.id).first
 
     @profile = Profile.where(user_id: current_user.id).first
-
-    @reviews = Review.where(profile_id: @profile.id)
-    sum_of_reviews = @reviews.sum(:rating).to_f
-    total_reviews = @reviews.length.to_f
-    @total_rating = total_reviews > 0 ? sum_of_reviews / @reviews.length : "No reviews"
+    if @profile.present?
+      @reviews = Review.where(profile_id: @profile.id)
+      sum_of_reviews = @reviews.sum(:rating).to_f
+      total_reviews = @reviews.length.to_f
+      @total_rating = total_reviews > 0 ? sum_of_reviews / @reviews.length : "No reviews"
+    end
   end
 
   def about
