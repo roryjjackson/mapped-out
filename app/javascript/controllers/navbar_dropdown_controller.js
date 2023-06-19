41731853b8bd0142a8dd40cd710793eb092ed5da
rory jackson <roryjamesjackson@hotmail.com>
2023-06-19 18:35:49 +0100

    messy

--- app/javascript/controllers/navbar_dropdown_controller.js
+++ app/javascript/controllers/navbar_dropdown_controller.js
@@ -5,18 +5,26 @@ export default class extends Controller {
 
   connect() {
     console.log("connected to navbar_dropdown");
-    if (window.innerWidth <= 950) {
-    console.log("connected to inner width big");
-    this.navbarMenu.style.display = 'block';
-    this.navbarContent.style.display = "none";
 
-  } else {
-    console.log("connected to inner width small");
-    this.navbarMenu.style.display = 'none';
-    this.navbarContent.style.display = "flex";
-
-    }
+    this.handleWindowSize();
+    // window.addEventListener('resize', this.handleWindowSize.bind(this));
   }
+
+  disconnect() {
+    window.removeEventListener('resize', this.handleWindowSize.bind(this));
+  }
+
+  // handleWindowSize() {
+  //   if (window.innerWidth >= 950) {
+  //     console.log("connected to inner width big");
+  //     this.navbarMenu.style.display = 'block';
+  //     this.navbarContent.style.display = 'none';
+  //   } else {
+  //     console.log("connected to inner width small");
+  //     this.navbarMenu.style.display = 'none';
+  //     this.navbarContent.style.display = 'flex';
+  //   }
+  // }
 
   openDropdown() {
     console.log("connected to open dropdown");
