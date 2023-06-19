81baa3e00f5b48d753ade6c8d557adfb0c3b5236
rory jackson <roryjamesjackson@hotmail.com>
2023-06-19 19:23:52 +0100

    navbar built and ready

--- app/javascript/controllers/navbar_dropdown_controller.js
+++ app/javascript/controllers/navbar_dropdown_controller.js
@@ -5,31 +5,12 @@ export default class extends Controller {
 
   connect() {
     console.log("connected to navbar_dropdown");
-
-    this.handleWindowSize();
-    // window.addEventListener('resize', this.handleWindowSize.bind(this));
   }
-
-  disconnect() {
-    window.removeEventListener('resize', this.handleWindowSize.bind(this));
-  }
-
-  // handleWindowSize() {
-  //   if (window.innerWidth >= 950) {
-  //     console.log("connected to inner width big");
-  //     this.navbarMenu.style.display = 'block';
-  //     this.navbarContent.style.display = 'none';
-  //   } else {
-  //     console.log("connected to inner width small");
-  //     this.navbarMenu.style.display = 'none';
-  //     this.navbarContent.style.display = 'flex';
-  //   }
-  // }
 
   openDropdown() {
     console.log("connected to open dropdown");
-
-    this.navbarContentTarget.style.display = 'block'
+    this.navbarMenuTarget.classList.toggle('display-block');
+    this.navbarContentTarget.classList.toggle('display-block');
     this.navbarTarget.classList.toggle('open-dropdown');
   }
 }
