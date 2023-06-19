fc49a059f87d21dc87691405dd9e2bd3f856f3bd
rory jackson <roryjamesjackson@hotmail.com>
2023-06-19 12:12:27 +0100

    playing with adding scrn width functionailty in stimilu

--- app/javascript/controllers/navbar_dropdown_controller.js
+++ app/javascript/controllers/navbar_dropdown_controller.js
@@ -5,6 +5,17 @@ export default class extends Controller {
 
   connect() {
     console.log("connected to navbar_dropdown");
+    if (window.innerWidth <= 950) {
+    console.log("connected to inner width big");
+    this.navbarMenu.style.display = 'block';
+    this.navbarContent.style.display = "none";
+
+  } else {
+    console.log("connected to inner width small");
+    this.navbarMenu.style.display = 'none';
+    this.navbarContent.style.display = "flex";
+
+    }
   }
 
   openDropdown() {
