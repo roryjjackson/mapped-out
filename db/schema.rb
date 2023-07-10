1a5713e5b7b8245e1dab175c9a13fc30e9e975a8
rory jackson <roryjamesjackson@hotmail.com>
2023-07-10 16:43:24 +0100

    editing tect and sttyle on mentor application page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_03_104307) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_142929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,8 +78,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_104307) do
     t.string "occupation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_mentors_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -186,7 +184,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_104307) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "chatrooms", "users"
-  add_foreign_key "mentors", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
   add_foreign_key "profiles", "users"
