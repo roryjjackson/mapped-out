38c63cdbcdb856b8c0b4994994b0480900cb9713
rory jackson <roryjamesjackson@hotmail.com>
2023-07-11 11:32:07 +0100

    text and css tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :chatrooms, only: %i[show create new index] do
-    resources :messages, only: :create
-  end
+  # resources :chatrooms, only: %i[show create new index] do
+  #   resources :messages, only: :create
+  # end
 
   get 'dashboard', to: 'pages#dashboard'
   get 'about', to: 'pages#about'
