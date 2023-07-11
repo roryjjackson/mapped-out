7610389615fde2fe1bc71080f2a6d06a28e9101f
rory jackson <roryjamesjackson@hotmail.com>
2023-07-11 12:53:12 +0100

    added multiple photos

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,12 +1,12 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  has_one_attached :photo, dependent: :destroy
+  has_many_attached :photos, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
   validates :user_id, uniqueness: true
   validates :name, :hours, :title, :summary, :personal_background, :compensation,
             :time_off, :entry_qualifications,
-            :q1, :q2, :q3, :q4, :q5, :q6, :q7, :q8, :q9, :photo, :address, presence: true
+            :q1, :q2, :q3, :q4, :q5, :q6, :q7, :q8, :q9, :photos, :address, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
